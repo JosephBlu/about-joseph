9f88eefe3e812abe2210a945d30c8ee98b0b7e02
Noel Cothren <noelcothren@gmail.com>
2017-01-10 09:27:47 -0700

    added style directory

--- index.php
+++ index.php
@@ -2,11 +2,11 @@
 <html>
 	<head>
 		<title>About Joseph Ramirez</title>
-		<link rel="stylesheet" type="text/css" href="style.css">
+		<link rel="stylesheet" type="text/css" href="style/style.css">
 	</head>
 	<body>
-		<h1 align="center">If you have five seconds to spare,</h1>
-		<h2 align="center">I'll tell you the story of my life...</h2>
+		<h1>If you have five seconds to spare,</h1>
+		<h2>I'll tell you the story of my life...</h2>
 		<p> My name is Joseph Ramirez. I am an easy going guy and yet an ambitious and competitive kind of person, but I always tend to be patient and calm. Stress is something that very rarely gets to me, but when it does I exhale and carry on and try to get over the obstacle. After all, I never put myself through stress situations unless there is a goal I have in mind.  </p>
 		<p>I have a lot of interests that I like to talk and learn about, such as cars, Fine Art, music, photography, or learning new skills or trades that I find interesting. </p>
 <img src="/images/emmablu.jpg" width="auto" height="350">
