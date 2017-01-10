51d8b06fe6628c168ac47bf7a67ae525e3feefa3
Noel Cothren <noelcothren@gmail.com>
2017-01-10 09:32:43 -0700

    uploaded image to server

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
 		<h2>I'll tell you the story of my life...</h2>
 		<p> My name is Joseph Ramirez. I am an easy going guy and yet an ambitious and competitive kind of person, but I always tend to be patient and calm. Stress is something that very rarely gets to me, but when it does I exhale and carry on and try to get over the obstacle. After all, I never put myself through stress situations unless there is a goal I have in mind.  </p>
 		<p>I have a lot of interests that I like to talk and learn about, such as cars, Fine Art, music, photography, or learning new skills or trades that I find interesting. </p>
-<img src="/images/emmablu.jpg" width="auto" height="350">
+<img src="images/emmablu.jpg" width="auto" height="350">
 		<p>Above all, my biggest interest these days is my daughter. EmmaBlu is such a life changer for me and my single greatest motivator to constantly improve myself. 	 </p>
 	</body>
 </html>
