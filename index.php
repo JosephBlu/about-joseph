5e600679d53bdc5f95dd05a2edc3355a64484e61
Noel Cothren <noelcothren@gmail.com>
2017-01-10 09:53:57 -0700

    text changes

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 	<body>
 		<h1>If you have five seconds to spare,</h1>
 		<h2>I'll tell you the story of my life...</h2>
-		<p> My name is Joseph Ramirez. My favorite color is pink. I am <strike>an easy going guy and yet </strike>an ambitious and competitive kind of person, and I always tend to be <strike>patient and calm</strike>loud and obnoxious. Stress is something that <strike>very rarely</strike> always gets to me, but when it does I scream and try to kill the offending person. I never put myself through stress situations unless there is a goal I have in mind.  </p>
+		<p> My name is Joseph Ramirez. My favorite color is pink. I am <strike>an easy going guy and yet </strike>an ambitious and competitive kind of person, and I always tend to be <strike>patient and calm</strike>loud and obnoxious. Stress is something that <strike>very rarely</strike> always gets to me, but when it does I scream and try to kill the offending person. I never put myself through stressful situations unless there is a goal I have in mind.  </p>
 		<p><img src="images/emmablu.jpg" width="auto" height="350">
 			I have a lot of interests that I like to talk and learn about, such as cars, Fine Art, music, photography, or learning new skills or trades that I find interesting.
 		Above all, my biggest interest these days is my daughter. EmmaBlu is such a life changer for me and my single greatest motivator to constantly improve myself. 	 </p>
